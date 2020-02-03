ae0a0cf8a6d413f4181972cba42683bf21ee7ac5
Jarno van Lierop <24877@ma-web.nl>
2020-02-03 21:58:04 +0100

    first

---
+++ MatchData/Assets/Scripts/matchData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LitJson;
+using System;
+
+[Serializable]
+public class matchData : MonoBehaviour
+{
+    private string jsonString;
+    private JsonData fileData;
+
+    private void Start()
+    {
+        jsonString = File.ReadAllText( Application.dataPath + "/Resources/match_data.json");
+        fileData = JsonMapper.ToObject(jsonString);
+        Debug.Log(fileData);
+    }
+
+}
